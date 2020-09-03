df739b78784f8dae55e40a6ff3ba54813a9153e3
Oliver Carlson <oswain@gmail.com>
2020-09-03 10:01:38 -0700

    added faster solution for 13 based off of garyguans work

--- 13.romanToInteger.js
+++ 13.romanToInteger.js
@@ -24,70 +24,89 @@ Given a roman numeral, convert it to an integer. Input is guaranteed to be withi
 
 const romanToInt = (s) => {
 	let res = 0;
-	let arr = s.split('');
-	for (let i = 0; i < arr.length; i++) {
-		if (arr[i] === 'I') {
-			if (arr[1 + i] === 'V') {
-				res += 4;
-				i++;
-				continue;
-			}
-			if (arr[1 + i] === 'X') {
-				res += 9;
-				i++;
-				continue;
-			} else {
-				res += 1;
-				continue;
-			}
-		}
-		if (arr[i] === 'X') {
-			if (arr[1 + i] === 'L') {
-				res += 40;
-				i++;
-				continue;
-			}
-			if (arr[1 + i] === 'C') {
-				res += 90;
-				i++;
-				continue;
-			} else {
-				res += 10;
-				continue;
-			}
-		}
-		if (arr[i] === 'C') {
-			if (arr[1 + i] === 'D') {
-				res += 400;
-				i++;
-				continue;
-			}
-			if (arr[1 + i] === 'M') {
-				res += 900;
-				i++;
-				continue;
-			} else {
-				res += 100;
-				continue;
-			}
-		}
-		if (arr[i] === 'V') {
-			res += 5;
-		}
-		if (arr[i] === 'L') {
-			res += 50;
-		}
-		if (arr[i] === 'D') {
-			res += 500;
-		}
-		if (arr[i] === 'M') {
-			res += 1000;
-		}
+	const symbol = {
+		I: 1,
+		V: 5,
+		X: 10,
+		L: 50,
+		C: 100,
+		D: 500,
+		M: 1000,
+	};
+	for (let i = 0; i < s.length; i++) {
+		symbol[s[i]] < symbol[s[1 + i]] ? (res -= symbol[s[i]]) : (res += symbol[s[i]]);
+		console.log(symbol[s[i]]);
+		console.log('symbol[s[1+i]] is ' + symbol[s[1 + i]]);
 	}
 	return res;
 };
-console.log(romanToInt('III')); // returns 3
-console.log(romanToInt('IV')); // returns 4
-console.log(romanToInt('IX')); // returns 9
-console.log(romanToInt('LVIII')); // returns 58
+// console.log(romanToInt('III')); // returns 3
+// console.log(romanToInt('IV')); // returns 4
+// console.log(romanToInt('IX')); // returns 9
+// console.log(romanToInt('LVIII')); // returns 58
 console.log(romanToInt('MCMXCIV')); // returns 1994
+
+// const romanToInt = (s) => {
+// 	let res = 0;
+// 	let arr = s.split('');
+// 	for (let i = 0; i < arr.length; i++) {
+// 		if (arr[i] === 'I') {
+// 			if (arr[1 + i] === 'V') {
+// 				res += 4;
+// 				i++;
+// 				continue;
+// 			}
+// 			if (arr[1 + i] === 'X') {
+// 				res += 9;
+// 				i++;
+// 				continue;
+// 			} else {
+// 				res += 1;
+// 				continue;
+// 			}
+// 		}
+// 		if (arr[i] === 'X') {
+// 			if (arr[1 + i] === 'L') {
+// 				res += 40;
+// 				i++;
+// 				continue;
+// 			}
+// 			if (arr[1 + i] === 'C') {
+// 				res += 90;
+// 				i++;
+// 				continue;
+// 			} else {
+// 				res += 10;
+// 				continue;
+// 			}
+// 		}
+// 		if (arr[i] === 'C') {
+// 			if (arr[1 + i] === 'D') {
+// 				res += 400;
+// 				i++;
+// 				continue;
+// 			}
+// 			if (arr[1 + i] === 'M') {
+// 				res += 900;
+// 				i++;
+// 				continue;
+// 			} else {
+// 				res += 100;
+// 				continue;
+// 			}
+// 		}
+// 		if (arr[i] === 'V') {
+// 			res += 5;
+// 		}
+// 		if (arr[i] === 'L') {
+// 			res += 50;
+// 		}
+// 		if (arr[i] === 'D') {
+// 			res += 500;
+// 		}
+// 		if (arr[i] === 'M') {
+// 			res += 1000;
+// 		}
+// 	}
+// 	return res;
+// };
