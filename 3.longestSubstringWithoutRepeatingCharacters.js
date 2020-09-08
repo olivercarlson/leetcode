61b27933414d87df54bdb57159a1d07c689c2cc9
Oliver Carlson <oswain@gmail.com>
2020-09-08 08:25:02 -0700

    added faster solution with sliding window for 3.longestSubstring

--- 3.longestSubstringWithoutRepeatingCharacters.js
+++ 3.longestSubstringWithoutRepeatingCharacters.js
@@ -19,25 +19,53 @@
 // 0 <= s.length <= 5 * 104
 // s consists of English letters, digits, symbols and spaces.
 
+//SLOW Bruteforce.
+// const lengthOfLongestSubstring = (s) => {
+// 	if (s.length === 0) return 0;
+// 	if (s.length === 1) return 1;
+
+// 	let res = 0;
+// 	let temp = [s[0]];
+// 	let i = 0;
+// 	let j = 0;
+
+// 	while (i < s.length && j < s.length) {
+// 		if (!temp.includes(s[j + 1])) {
+// 			res > temp.length ? (res = res) : (res = temp.length);
+// 			temp.push(s[j + 1]);
+// 			j++;
+// 		} else {
+// 			res > temp.length ? (res = res) : (res = temp.length);
+// 			i++;
+// 			j = i;
+// 			temp = [s[i]];
+// 		}
+// 	}
+// 	return res;
+// };
+
+//convert to sliding window using a set.
 const lengthOfLongestSubstring = (s) => {
 	if (s.length === 0) return 0;
 	if (s.length === 1) return 1;
 
 	let res = 0;
-	let temp = [s[0]];
 	let i = 0;
 	let j = 0;
 
-	while (i < s.length && j < s.length) {
-		if (!temp.includes(s[j + 1])) {
-			res > temp.length ? (res = res) : (res = temp.length);
-			temp.push(s[j + 1]);
+	// init set, add first letter.
+	let set = new Set();
+	set.add(s[0]);
+
+	// set up the loop, stop if either i becomes the last letter or j+1 becomes the last letter.
+	while (i < s.length && j < s.length - 1) {
+		if (!set.has(s[j + 1])) {
+			set.add(s[j + 1]);
+			res = Math.max(res, set.size);
 			j++;
 		} else {
-			res > temp.length ? (res = res) : (res = temp.length);
+			set.delete(s[i]);
 			i++;
-			j = i;
-			temp = [s[i]];
 		}
 	}
 	return res;
