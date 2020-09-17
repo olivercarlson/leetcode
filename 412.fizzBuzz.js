4d71eb9cf7d7ea61233344c824c1fd91fb71e82b
Oliver Carlson <oswain@gmail.com>
2020-09-17 08:09:15 -0700

    start 206

--- 412.fizzBuzz.js
+++ 412.fizzBuzz.js
@@ -1,54 +1,15 @@
-// Write a program that outputs the string representation of numbers from 1 to n.
-
-// But for multiples of three it should output “Fizz” instead of the number and for the multiples of five output “Buzz”. For numbers which are multiples of both three and five output “FizzBuzz”.
-
-// Example:
-
-// n = 15,
-// Return:
-// [
-//     "1",
-//     "2",
-//     "Fizz",
-//     "4",
-//     "Buzz",
-//     "Fizz",
-//     "7",
-//     "8",
-//     "Fizz",
-//     "Buzz",
-//     "11",
-//     "Fizz",
-//     "13",
-//     "14",
-//     "FizzBuzz"
-// ]
-
-/**
- * @param {number} n
- * @return {string[]}
- */
-const fizzBuzz = (n) => {
-	const res = [];
+var fizzBuzz = function (n) {
+	let res = Array(n);
 	for (let i = 1; i <= n; i++) {
-		// if (i === 0) {
-		// 	res.push(`${i}`);
-		// 	continue;
-		// }
 		if (i % 5 === 0 && i % 3 === 0) {
-			res.push('FizzBuzz');
-			continue;
-		}
-		if (i % 5 === 0) {
-			res.push('Buzz');
-		}
-		if (i % 3 === 0) {
-			res.push('Fizz');
+			res[i - 1] = 'FizzBuzz';
+		} else if (i % 5 === 0) {
+			res[i - 1] = 'Buzz';
+		} else if (i % 3 === 0) {
+			res[i - 1] = 'Fizz';
 		} else {
-			res.push(`${i}`);
+			res[i - 1] = i.toString();
 		}
 	}
 	return res;
 };
-
-console.log(fizzBuzz(1));
