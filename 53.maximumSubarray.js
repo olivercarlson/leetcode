9d5b19c2ef3fc40d67d2830b2dae90a690215243
Oliver Carlson <oswain@gmail.com>
2020-09-17 10:09:49 -0700

    fixed solution to 53

--- 53.maximumSubarray.js
+++ 53.maximumSubarray.js
@@ -10,22 +10,22 @@
 // -231 <= nums[i] <= 231 - 1
 
 const maxSubArray = (nums) => {
-	let max1 = nums[0];
-	let max2 = nums[0];
-	for (let i = 1; i < nums.length; i++) {
-		max1 = Math.max(nums[i], max1 + nums[i]);
-		console.log(max1);
-		if (max1 > max2) {
-			max2 = max1;
-		}
+	if (nums.length === 0) {
+		return nums[0];
+	}
+	let max1 = 0,
+		max2 = nums[0];
+	for (let i = 0; i < nums.length; i++) {
+		max1 += nums[i];
+		max1 = Math.max(max1, nums[i]);
+		max1 > max2 ? (max2 = max1) : (max2 = max2);
 	}
 	return max2;
 };
-
-console.log(maxSubArray([-2, 1, -3, 4, -1, 2, 1, -5, 4])); // returns 6 (4,-1,2,1)
+// console.log(maxSubArray([-2, 1, -3, 4, -1, 2, 1, -5, 4])); // returns 6 (4,-1,2,1)
 // console.log(maxSubArray([-2, -1])); // returns -1;
 // console.log(maxSubArray([-1, -2])); // returns -1;
 // console.log(maxSubArray([-2, 1])); // return 1;
 // console.log(maxSubArray([1])); // returns 1 (1)
 // console.log(maxSubArray([0])); // returns 0 (1)
-// console.log(maxSubArray([-1])); // returns -1 (1);
+console.log(maxSubArray([-1])); // returns -1 (1);
