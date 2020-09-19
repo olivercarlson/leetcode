4bb50d6ff39885f2f029e207d09c0550fdd18000
Oliver Carlson <oswain@gmail.com>
2020-09-19 08:40:50 -0700

    start 171

---
+++ 171.excelSheetColumnNumbers.js
@@ -0,0 +1,28 @@
+// Given a column title as appear in an Excel sheet, return its corresponding column number.
+
+// For example:
+
+//     A -> 1
+//     B -> 2
+//     C -> 3
+//     ...
+//     Z -> 26
+//     AA -> 27
+//     AB -> 28
+//     ...
+
+// Constraints:
+
+//     1 <= s.length <= 7
+//     s consists only of uppercase English letters.
+//     s is between "A" and "FXSHRXW".
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var titleToNumber = function (s) {};
+
+console.log(titleToNumber('A')); // 1
+console.log(titleToNumber('AB')); // 28
+console.log(titleToNumber('ZY')); // 701
