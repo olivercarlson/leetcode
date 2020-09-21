466f908faa2ecdc94fc9cbc20daf6479ec380fa7
Oliver Carlson <oswain@gmail.com>
2020-09-21 09:53:17 -0700

    added initial solution to 387

--- 387.firstUniqueCharacterInString.js
+++ 387.firstUniqueCharacterInString.js
@@ -17,9 +17,26 @@
  */
 
 const firstUniqChar = (s) => {
-	//
-	//
+	const key = {};
+
+	for (let i = 0; i < s.length; i++) {
+		if (!key[s[i]]) {
+			key[s[i]] = { count: 1, firstIndex: i };
+		} else {
+			key[s[i]].count++;
+		}
+	}
+
+	const arr = s.split('');
+	return arr.reduce((acc, curr, i) => {
+		if (key[curr].count === 1 && acc === -1) return key[curr].firstIndex;
+		if (key[curr].count === 1) return acc < key[curr].firstIndex ? acc : key[curr].firstIndex;
+		return acc;
+	}, -1);
 };
 
-console.log(firstUniqChar('leetcode')); // index = 0
+// console.log(firstUniqChar('leetcode')); // index = 0
 console.log(firstUniqChar('loveleetcode')); // index = 2
+
+// console.log(`curr is ${curr} and verdict is ${key[curr].count === 1}`);
+// console.log(`acc is ${acc}`);
