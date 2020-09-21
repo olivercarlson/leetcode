01475af0907abf4dfc2baadd1b6e0d1c345dc5c6
Oliver Carlson <oswain@gmail.com>
2020-09-20 19:24:36 -0700

    start 387

---
+++ 387.firstUniqueCharacterInString.js
@@ -0,0 +1,25 @@
+// 387. First Unique Character in a String
+// Easy
+
+// Given a string, find the first non-repeating character in it and return its index. If it doesn't exist, return -1.
+
+// Examples:
+
+// s = "leetcode"
+// return 0.
+
+// s = "loveleetcode"
+// return 2.
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+
+const firstUniqChar = (s) => {
+	//
+	//
+};
+
+console.log(firstUniqChar('leetcode')); // index = 0
+console.log(firstUniqChar('loveleetcode')); // index = 2
