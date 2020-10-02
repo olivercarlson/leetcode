b823872eaf8d89f525cee0ae0304ac6d0ba374b5
Oliver Carlson <oswain@gmail.com>
2020-10-02 11:34:54 -0700

    initial solution for 155

--- 155.minStack.js
+++ 155.minStack.js
@@ -1,28 +1,57 @@
 /**
  * initialize your data structure here.
  */
-var MinStack = function () {};
+
+class MinStack {
+	constructor() {
+		this.stack = [];
+		this.min = [];
+	}
+	getMin() {
+		return this.min[this.min.length - 1];
+	}
+
+	push(x) {
+		!this.min.length ? this.min.push(x) : this.min.push(Math.min(x, this.getMin()));
+		this.stack.push(x);
+	}
+	pop() {
+		this.min.pop();
+		return this.stack.pop();
+	}
+	top() {
+		return this.stack[this.stack.length - 1];
+	}
+}
 
 /**
  * @param {number} x
  * @return {void}
  */
-MinStack.prototype.push = function (x) {};
+MinStack.prototype.push = function (x) {
+	return this.push(n);
+};
 
 /**
  * @return {void}
  */
-MinStack.prototype.pop = function () {};
+MinStack.prototype.pop = function () {
+	return this.pop();
+};
 
 /**
  * @return {number}
  */
-MinStack.prototype.top = function () {};
+MinStack.prototype.top = function () {
+	// ?
+};
 
 /**
  * @return {number}
  */
-MinStack.prototype.getMin = function () {};
+MinStack.prototype.getMin = function () {
+	//Math.min()
+};
 
 /**
  * Your MinStack object will be instantiated and called as such:
