99169a335070a2b476f493ceee2c007740afde34
Oliver Carlson <oswain@gmail.com>
2020-11-03 07:13:47 -0800

    added non-cheating solution for reverseStr

--- interviewCake/reverseStr.js
+++ interviewCake/reverseStr.js
@@ -1,4 +1,23 @@
+// const reverse = (arrayOfChars) => {
+// 	// Reverse the input array of characters in place
+// 	return arrayOfChars.reverse();
+// };
+
+// without cheating:
 const reverse = (arrayOfChars) => {
-	// Reverse the input array of characters in place
-	return arrayOfChars.reverse();
+	if (arrayOfChars.length <= 1) return arrayOfChars;
+
+	let temp = '';
+	let i = 0;
+	while (i < arrayOfChars.length / 2) {
+		left = arrayOfChars[i];
+		right = arrayOfChars[arrayOfChars.length - (1 + i)];
+		temp = left;
+		arrayOfChars[i] = right;
+		arrayOfChars[arrayOfChars.length - (1 + i)] = temp;
+		i++;
+	}
+	return arrayOfChars;
 };
+
+console.log(reverse(['a', 'b', 'c', 'd']));
