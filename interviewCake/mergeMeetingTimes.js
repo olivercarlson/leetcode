0356c9b8f7229c8b1692b5784072f77961090069
Oliver Carlson <oswain@gmail.com>
2020-11-03 07:05:19 -0800

    reverse str in place

--- interviewCake/mergeMeetingTimes.js
+++ interviewCake/mergeMeetingTimes.js
@@ -66,12 +66,12 @@ const mergeRanges = (meetings) => {
 // 	])
 // );
 
-console.log(
-	mergeRanges([
-		{ startTime: 0, endTime: 1 },
-		{ startTime: 3, endTime: 5 },
-		{ startTime: 4, endTime: 8 },
-		{ startTime: 10, endTime: 12 },
-		{ startTime: 9, endTime: 10 },
-	])
-);
+// console.log(
+// 	mergeRanges([
+// 		{ startTime: 0, endTime: 1 },
+// 		{ startTime: 3, endTime: 5 },
+// 		{ startTime: 4, endTime: 8 },
+// 		{ startTime: 10, endTime: 12 },
+// 		{ startTime: 9, endTime: 10 },
+// 	])
+// );
---
+++ interviewCake/reverseStr.js
@@ -0,0 +1,4 @@
+const reverse = (arrayOfChars) => {
+	// Reverse the input array of characters in place
+	return arrayOfChars.reverse();
+};
