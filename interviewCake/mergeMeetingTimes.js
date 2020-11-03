33328f99b29468757649f3ffe87a30325ba799bc
Oliver Carlson <oswain@gmail.com>
2020-11-03 06:57:07 -0800

    added significantly improved solution for merge meeting times

--- interviewCake/mergeMeetingTimes.js
+++ interviewCake/mergeMeetingTimes.js
@@ -36,77 +36,33 @@ const mergeRanges = (meetings) => {
 	const meetingsCopy = JSON.parse(JSON.stringify(meetings));
 	const sortedMeetings = meetingsCopy.sort((a, b) => a.startTime - b.startTime);
 	const mergedMeetings = [sortedMeetings[0]];
+	let currentMeeting;
 	for (let i = 1; i < sortedMeetings.length; i++) {
-		const currentMeeting = sortedMeetings[i];
-		const lastMergedMeeting = mergedMeetings[mergedMeetings.length - 1];
-
-		if (currentMeeting.startTime <= lastMergedMeeting.endTime) {
-			lastMergedMeeting.endTime = Math.max(lastMergedMeeting.endTime, currentMeeting.endTime);
+		currentMeeting = sortedMeetings[i];
+		if (mergedMeetings[mergedMeetings.length - 1].endTime >= currentMeeting.startTime) {
+			mergedMeetings[mergedMeetings.length - 1].endTime = Math.max(currentMeeting.endTime, mergedMeetings[mergedMeetings.length - 1].endTime);
 		} else {
 			mergedMeetings.push(currentMeeting);
 		}
 	}
 	return mergedMeetings;
 };
-// const mergeRanges = (ranges) => {
-// 	let sortedStart = ranges.sort((a, b) => a['startTime'] - b['startTime']);
-// 	const res = [];
-// 	let temp = { startTime: 0, endTime: 0 };
 
-// 	for (let i = 0; i < sortedStart.length; i++) {
-// 		if (!sortedStart[i + 1]) {
-// 			// no temp
-// 			if (temp === { startTime: 0, endTime: 0 }) {
-// 				res.push({ startTime: sortedStart[i]['startTime'], endTime: sortedStart[i]['endTime'] });
-// 				return res;
-// 			}
-// 			// merge them
-// 			if (temp['endTime'] >= sortedStart[i]['startTime']) {
-// 				temp.startTime = temp.startTime === 0 ? sortedStart[i]['startTime'] : Math.min(temp.startTime, sortedStart[i]['startTime']);
-// 				temp.endTime = Math.max(temp.endTime, sortedStart[i]['endTime']);
-// 				res.push({ ...temp });
-// 				return res;
-// 			}
-// 			// don't merge, add both;
-// 			else {
-// 				res.push({ ...temp });
-// 				res.push({ startTime: sortedStart[i]['startTime'], endTime: sortedStart[i]['endTime'] });
-// 				return res;
-// 			}
-// 		}
-// 		if (!sortedStart[i + 1] && temp === { startTime: 0, endTime: 0 }) {
-// 			continue;
-// 		} else if (!sortedStart[i + 1]) {
-// 			res.push({ startTime: Math.min(temp['startTime'], sortedStart[i]['startTime']), endTime: Math.max(temp['endTime'], sortedStart[i]['endTime']) });
+// console.log(
+// 	mergeRanges([
+// 		{ startTime: 1, endTime: 3 },
+// 		{ startTime: 4, endTime: 8 },
+// 	])
+// );
 
-// 			continue;
-// 		} else if (temp['endTime'] >= sortedStart[i]['startTime']) {
-// 			temp.startTime = temp.startTime === 0 ? sortedStart[i]['startTime'] : Math.min(temp.startTime, sortedStart[i]['startTime']);
-// 			temp.endTime = Math.max(temp.endTime, sortedStart[i]['endTime']);
-// 			continue;
-// 		} else if (temp.startTime !== 0 || temp.endTime !== 0) {
-// 			res.push({ ...temp });
-// 		}
-// 		temp = { startTime: sortedStart[i]['startTime'], endTime: sortedStart[i]['endTime'] };
-// 	}
-// 	return res;
-// };
-
-console.log(
-	mergeRanges([
-		{ startTime: 1, endTime: 3 },
-		{ startTime: 4, endTime: 8 },
-	])
-);
-
-console.log(
-	mergeRanges([
-		{ startTime: 1, endTime: 10 },
-		{ startTime: 2, endTime: 6 },
-		{ startTime: 3, endTime: 5 },
-		{ startTime: 7, endTime: 9 },
-	])
-);
+// console.log(
+// 	mergeRanges([
+// 		{ startTime: 1, endTime: 10 },
+// 		{ startTime: 2, endTime: 6 },
+// 		{ startTime: 3, endTime: 5 },
+// 		{ startTime: 7, endTime: 9 },
+// 	])
+// );
 
 console.log(
 	mergeRanges([
@@ -117,5 +73,3 @@ console.log(
 		{ startTime: 9, endTime: 10 },
 	])
 );
-// [1-3],
-//
