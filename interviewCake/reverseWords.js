aff743dcca0444e6919d39ef82c4fbb2b2a407b0
Oliver Carlson <oswain@gmail.com>
2020-11-05 18:44:55 -0800

    added solution for reverseWords

--- interviewCake/reverseWords.js
+++ interviewCake/reverseWords.js
@@ -1,2 +1,19 @@
 // Decode the message by reversing the words
-const reverseWords = (message) => {};
+const reverseWords = (arrayOfChars) => {
+	if (arrayOfChars <= 1) return arrayOfChars;
+	const arrayOfWords = arrayOfChars.join('').split(' ');
+	let indexLeft = 0,
+		indexRight = arrayOfWords.length - 1;
+	while (indexLeft < indexRight) {
+		console.log(`${arrayOfWords[indexLeft]} ${arrayOfWords[indexRight]}`);
+		let temp = arrayOfWords[indexLeft];
+		arrayOfWords[indexLeft] = arrayOfWords[indexRight];
+		arrayOfWords[indexRight] = temp;
+		indexLeft++;
+		indexRight--;
+	}
+	return arrayOfWords;
+};
+
+const message = ['c', 'a', 'k', 'e', ' ', 'p', 'o', 'u', 'n', 'd', ' ', 's', 't', 'e', 'a', 'l'];
+console.log(reverseWords(message).join(' '));
