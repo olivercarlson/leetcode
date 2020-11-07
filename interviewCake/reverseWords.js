dd009405c054c4a80fea6660755f3d6d3e5fb7fd
Oliver Carlson <oswain@gmail.com>
2020-11-06 20:50:58 -0800

    added cheat solution for reverseWords

--- interviewCake/reverseWords.js
+++ interviewCake/reverseWords.js
@@ -1,25 +1,36 @@
 // Decode the message by reversing the words:
-// brute force:
+
+// cheating:
 const reverseWords = (arrayOfChars) => {
-	if (arrayOfChars <= 1) return arrayOfChars;
-	const arrayOfWords = arrayOfChars.join('').split(' ');
-	let indexLeft = 0,
-		indexRight = arrayOfWords.length - 1;
-	while (indexLeft < indexRight) {
-		console.log(`${arrayOfWords[indexLeft]} ${arrayOfWords[indexRight]}`);
-		let temp = arrayOfWords[indexLeft];
-		arrayOfWords[indexLeft] = arrayOfWords[indexRight];
-		arrayOfWords[indexRight] = temp;
-		indexLeft++;
-		indexRight--;
-	}
-	return arrayOfWords;
+	return arrayOfChars.join('').split(' ').reverse();
 };
 
+// brute force:
+// const reverseWords = (arrayOfChars) => {
+// 	if (arrayOfChars <= 1) return arrayOfChars;
+// 	const arrayOfWords = arrayOfChars.join('').split(' ');
+// 	let indexLeft = 0,
+// 		indexRight = arrayOfWords.length - 1;
+// 	while (indexLeft < indexRight) {
+// 		console.log(`${arrayOfWords[indexLeft]} ${arrayOfWords[indexRight]}`);
+// 		let temp = arrayOfWords[indexLeft];
+// 		arrayOfWords[indexLeft] = arrayOfWords[indexRight];
+// 		arrayOfWords[indexRight] = temp;
+// 		indexLeft++;
+// 		indexRight--;
+// 	}
+// 	return arrayOfWords;
+// };
+
 // O(1) space AKA In place swap:
-const reverseWords = (arrayOfChars) => {
-	return arrayOfChars;
-};
+// const reverseWords = (arrayOfChars) => {
+// 	if (arrayOfChars <= 1) return arrayOfChars;
+
+// 	let indexLeft = 0,
+// 		indexRight = arrayOfChars.length;
+
+// 	return arrayOfChars;
+// };
 
 const message = ['c', 'a', 'k', 'e', ' ', 'p', 'o', 'u', 'n', 'd', ' ', 's', 't', 'e', 'a', 'l'];
 console.log(reverseWords(message).join(' '));
